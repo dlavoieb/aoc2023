c8281cd09625a6bed609d6e89fce7d146e81b162
David Lavoie-Boutin <david.lavoie.boutin@zeitview.com>
2024-06-21 10:25:35 -0400

    Solve day09 part 2

--- day-09/src/main.rs
+++ day-09/src/main.rs
@@ -1,6 +1,11 @@
 use std::str::FromStr;
 
 fn main() {
+    part1();
+    part2();
+}
+
+fn part1() {
     let lines = utils::read_file("day-09/input.txt");
     let mut extrapolated_values = Vec::new();
     for line in lines {
@@ -9,7 +14,20 @@ fn main() {
         let extrapolated_value = extrapolate_line(history);
         extrapolated_values.push(extrapolated_value);
     }
-    let sum : i32 = extrapolated_values.iter().sum();
+    let sum: i32 = extrapolated_values.iter().sum();
+    println!("Sum: {sum}");
+}
+
+fn part2() {
+    let lines = utils::read_file("day-09/input.txt");
+    let mut extrapolated_values = Vec::new();
+    for line in lines {
+        let line: Vec<i32> = line.split(" ").map(|c| i32::from_str(c).unwrap()).collect();
+        let history = create_history(line);
+        let extrapolated_value = extrapolate_backwards(history);
+        extrapolated_values.push(extrapolated_value);
+    }
+    let sum: i32 = extrapolated_values.iter().sum();
     println!("Sum: {sum}");
 }
 
@@ -26,6 +44,16 @@ fn extrapolate_line(mut history: Vec<Vec<i32>>) -> i32 {
     let mut previous_value = 0;
     for rline in history.iter_mut().rev() {
         previous_value = rline.last().unwrap() + previous_value;
+        rline.push(previous_value);
+    }
+    let extrapolated_value = *history[0].last().unwrap();
+    extrapolated_value
+}
+
+fn extrapolate_backwards(mut history: Vec<Vec<i32>>) -> i32 {
+    let mut previous_value = 0;
+    for rline in history.iter_mut().rev() {
+        previous_value = rline.first().unwrap() - previous_value;
         rline.push(previous_value);
     }
     let extrapolated_value = *history[0].last().unwrap();
